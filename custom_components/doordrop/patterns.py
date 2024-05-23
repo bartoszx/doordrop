b7fa612fdb44801c96a8e992dbf7dfdef076db3a
bartosz aninowski <bartoszx@gamil.com>
2024-05-23 20:32:15 +0200

    - Improved pattern matching for DPD

--- custom_components/doordrop/patterns.py
+++ custom_components/doordrop/patterns.py
@@ -2,15 +2,14 @@
 PATTERNS = {
     'Allegro': r'A000[a-zA-Z0-9]{6}',
     'InPost': r'\d{24}',
-    'DPD': r'1000727398456[U]\d{13}',
+    'DPD': r'10007\d{9}U',
     'DHL': [
-        r'JJD\d{21}',  # Wzorzec dla DHL z numerem o stałej długości 21
-        r'\d{10}',     # Alternatywny wzorzec dla DHL
-        r'000\d{21}'   # Inny alternatywny wzorzec dla DHL
+        r'JJD\d{21}',  # Preferred pattern for DHL
+        r'\d{10}',     # Alternative pattern for DHL
+        r'000\d{21}'   # Another alternative pattern for DHL
     ],
     'Pocztex': r'PX\d{10}',
-    'Poczta Polska': r'\d{20}',  # Wzorzec dla Poczty Polskiej
-    'Generic13Digit': r'\d{13}',
-    'Generic12Digit': r'\d{12}'
+    'Poczta Polska': r'\d{20}',  # Pattern for Poczta Polska
+    'Generic13Digit': r'\d{13}', # Generic 13-digit pattern
+    'Generic12Digit': r'\d{12}'  # Generic 12-digit pattern
 }
-
