b0ecde9b92bf2b2151fbb8ec712aedcd97d3e3a3
bartosz aninowski <bartoszx@gamil.com>
2024-05-23 20:45:50 +0200

    - Added specific patterns for DPD tracking numbers:   - 13 digits followed by any uppercase letter.   - 14-digit tracking numbers.   - 14-character alphanumeric codes (uppercase letters and digits). - Improved pattern matching for DHL and Poczta Polska to handle various formats. - Enhanced support for identifying multiple providers within email content. - Updated email processing logic to iterate through identified providers and find matching tracking codes. - Refactored tracking code search logic into a separate module (search_patterns.py). - Enhanced logging and error handling for better debugging and stability.

--- custom_components/doordrop/patterns.py
+++ custom_components/doordrop/patterns.py
@@ -2,14 +2,19 @@
 PATTERNS = {
     'Allegro': r'A000[a-zA-Z0-9]{6}',
     'InPost': r'\d{24}',
-    'DPD': r'10007\d{9}U',
+    'DPD': [
+        r'\d{13}[A-Z]',     # Numer przesyłki DPD kończący się dowolną literą
+        r'\d{14}',          # Numer przesyłki DPD składający się z 14 cyfr
+        r'[A-Z0-9]{14}'     # Numer przesyłki DPD zawierający 14 znaków (duże litery i cyfry)
+    ],
     'DHL': [
-        r'JJD\d{21}',  # Preferred pattern for DHL
-        r'\d{10}',     # Alternative pattern for DHL
-        r'000\d{21}'   # Another alternative pattern for DHL
+        r'JJD\d{21}',       # Wzorzec dla DHL z numerem o stałej długości 21
+        r'\d{10}',          # Alternatywny wzorzec dla DHL
+        r'000\d{21}'        # Inny alternatywny wzorzec dla DHL
     ],
     'Pocztex': r'PX\d{10}',
-    'Poczta Polska': r'\d{20}',  # Pattern for Poczta Polska
-    'Generic13Digit': r'\d{13}', # Generic 13-digit pattern
-    'Generic12Digit': r'\d{12}'  # Generic 12-digit pattern
+    'Poczta Polska': r'\d{20}',  # Wzorzec dla Poczty Polskiej
+    'Generic13Digit': r'\d{13}', # Ogólny wzorzec dla 13-cyfrowych numerów
+    'Generic12Digit': r'\d{12}'  # Ogólny wzorzec dla 12-cyfrowych numerów
 }
+
